9e6c5262c0e469d961f10c29696fca244859cd36
elggem <trilly@gmail.com>
2017-07-25 17:59:58 +0800

    CI fix

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-REQUIRED_PACKAGES = ['pandas', 'numpy', 'termcolor']
+REQUIRED_PACKAGES = ['pandas', 'numpy', 'termcolor', 'tensorflow-gpu']
 
 setup(name='trainer',
       version='0.1',
