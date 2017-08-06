a63806474ae3dd8c6b9482f8f23957d5670f507b
RalfMayet <trilly@gmail.com>
2017-08-06 13:59:53 +1000

    experiemental script

--- run_experiments_samplesize.sh
+++ run_experiments_samplesize.sh
@@ -7,18 +7,9 @@ for SAMPLESIZE in 1 2 4 8 16 32 64 128 256
 do
     for ITERATION in {1..10}
     do
-        echo python trainer/task.py --train-files ./data/filtered/output.* \
-                                    --job-dir ./output/samplesize/$SAMPLESIZE \
-                                    --sample_size $SAMPLESIZE \
-                                    --train-steps $(($ITERATION * $STEPSIZE))
-
-        echo python generate.py --samples 1000 \
-                                --out_path ./output/samplesize/notalk\_$SAMPLESIZE\_$(($ITERATION * $STEPSIZE)) 
-                                --dat_seed ./data/filtered/output.dat 
-                                output/samplesize/$SAMPLESIZE/model.ckpt-$(($ITERATION * $STEPSIZE))
-
-        cp output/samplesize/$SAMPLESIZE/model.ckpt-$(($ITERATION * $STEPSIZE)) \
-           output/samplesize/backup/$SAMPLESIZE-model.ckpt-$(($ITERATION * 50000))
+        python trainer/task.py --train-files ./data/filtered/output.* --job-dir ./output/samplesize/$SAMPLESIZE --sample_size $SAMPLESIZE --train-steps $(($ITERATION * $STEPSIZE))
+        python generate.py --samples 1000 --out_path ./output/samplesize/notalk\_$SAMPLESIZE\_$(($ITERATION * $STEPSIZE)) --dat_seed ./data/filtered/output.dat output/samplesize/$SAMPLESIZE/model.ckpt-$(($ITERATION * $STEPSIZE))
+        cp output/samplesize/$SAMPLESIZE/model.ckpt-$(($ITERATION * $STEPSIZE))* output/samplesize/backup/$SAMPLESIZE-model.ckpt-$(($ITERATION * 50000))
     done
 done
 
