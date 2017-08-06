cc1ca19baad362ed7142f501ca0d6549e3cecfba
RalfMayet <trilly@gmail.com>
2017-08-06 14:09:38 +1000

    experiemental script

--- run_experiments_samplesize.sh
+++ run_experiments_samplesize.sh
@@ -1,16 +1,16 @@
 
-STEPSIZE=10  # 50000
+STEPSIZE=50000
 
 mkdir output/samplesize
-mkdir output/samplesize/backup
 
 for SAMPLESIZE in 1 2 4 8 16 32 64 128 256
 do
-    for ITERATION in {1..10}
+    for ITERATION in {2..10}
     do
         python trainer/task.py --train-files ./data/filtered/output.* --job-dir ./output/samplesize/$SAMPLESIZE --sample_size $SAMPLESIZE --train-steps $(($ITERATION * $STEPSIZE))
         python generate.py --samples 1000 --out_path ./output/samplesize/notalk\_$SAMPLESIZE\_$(($ITERATION * $STEPSIZE)) --dat_seed ./data/filtered/output.dat output/samplesize/$SAMPLESIZE/model.ckpt-$(($ITERATION * $STEPSIZE))
-        cp output/samplesize/$SAMPLESIZE/model.ckpt-$(($ITERATION * $STEPSIZE))* output/samplesize/backup/$SAMPLESIZE-model.ckpt-$(($ITERATION * 50000))
+        mkdir -p output/samplesize/backup/$SAMPLESIZE
+        cp output/samplesize/$SAMPLESIZE/model.ckpt-$(($ITERATION * $STEPSIZE))* output/samplesize/backup/$SAMPLESIZE/model.ckpt-$(($ITERATION * 50000))
     done
 done
 
